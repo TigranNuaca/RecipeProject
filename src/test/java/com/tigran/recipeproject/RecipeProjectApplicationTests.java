1f456d7436be18a0751e9af88fb76b08a4791b26
TigraNuaca <tigrannuaca2020@gmail.com>
2023-02-27 09:18:49 -0800

    Second Commit

---
+++ src/test/java/com/tigran/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigran.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
